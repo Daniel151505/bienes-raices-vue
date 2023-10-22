6bd511380869e30cc3ce86114b1f520c5e2643db
Daniel151505 <daniel150520011@gmail.com>
2023-10-22 18:37:27 -0500

    Cerrar sesión en Firebase

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,11 +1,17 @@
 import { ref, computed, onMounted } from "vue";
 import { defineStore } from "pinia";
 import { useFirebaseAuth } from "vuefire";
-import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { useRouter } from "vue-router";
 
 export const useAuthStore = defineStore("auth", () => {
   const auth = useFirebaseAuth();
   const authUser = ref(null);
+  const router = useRouter();
 
   const errorMsg = ref("");
   const errorCodes = {
@@ -25,9 +31,21 @@ export const useAuthStore = defineStore("auth", () => {
       .then((userCredential) => {
         const user = userCredential.user;
         authUser.value = user;
+        router.push({ name: "admin-propiedades" });
       })
       .catch((error) => {
         errorMsg.value = errorCodes[error.code];
+      });
+  };
+
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        authUser.value = null;
+        router.push({ name: "login" });
+      })
+      .catch((error) => {
+        console.log(error);
       });
   };
 
@@ -39,5 +57,5 @@ export const useAuthStore = defineStore("auth", () => {
     return authUser.value;
   });
 
-  return { login, hasError, errorMsg, isAuth };
+  return { login, logout, hasError, errorMsg, isAuth };
 });
