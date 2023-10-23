2d29ff9c06336c76dedfcaef65f7bb702bbd04d9
Daniel151505 <daniel150520011@gmail.com>
2023-10-22 19:13:43 -0500

    Permitir el acceso si el usuario esta autenticado

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { useFirebaseAuth } from "vuefire";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
@@ -24,19 +26,16 @@ const router = createRouter({
           path: "/admin/propiedades",
           name: "admin-propiedades",
           component: () => import("../views/admin/AdminView.vue"),
-          meta: { requiresAuth: true },
         },
         {
           path: "/admin/nueva",
           name: "nueva-propiedad",
           component: () => import("../views/admin/NuevaPropiedadView.vue"),
-          meta: { requiresAuth: true },
         },
         {
           path: "/admin/editar/:id",
           name: "editar-propiedad",
           component: () => import("../views/admin/EditarPropiedadView.vue"),
-          meta: { requiresAuth: true },
         },
       ],
     },
@@ -44,14 +43,35 @@ const router = createRouter({
 });
 
 // Guard de navegación
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   const requiresAuth = to.matched.some((url) => url.meta.requiresAuth);
   if (requiresAuth) {
     // Comprobar si el usuario esta autenticado
+    try {
+      await authenticateUser();
+      next();
+    } catch (error) {
+      console.log(error);
+      next({ name: "login" });
+    }
   } else {
     // No esta protegido, mostramos la vista
     next();
   }
 });
+
+function authenticateUser() {
+  const auth = useFirebaseAuth();
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe();
+      if (user) {
+        resolve(user);
+      } else {
+        reject();
+      }
+    });
+  });
+}
 
 export default router;
