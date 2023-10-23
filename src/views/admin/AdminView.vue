c9c7e7c5d8c2cc88d17993c910912f418c6c04ae
Daniel151505 <daniel150520011@gmail.com>
2023-10-22 19:51:53 -0500

    Creando el formulario de nuevas propiedades

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -1,6 +1,7 @@
-<script setup>
-
-</script>
+<script setup></script>
 <template>
-<h2 class="text-center text-h3 my-5 font-weight-bold">Admin Panel</h2>
+  <h2 class="text-center text-h3 my-5 font-weight-bold">Admin Panel</h2>
+  <v-btn color="blue" variant="flat" :to="{ name: 'nueva-propiedad' }">
+    Nueva Propiedad
+  </v-btn>
 </template>
