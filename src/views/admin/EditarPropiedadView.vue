be58c43aa59174d1d992a38742f8d3f14c5fc176
Daniel151505 <daniel150520011@gmail.com>
2023-10-23 20:41:03 -0500

    Mostrar la fotografia actual o la nueva en caso de actualizar

--- src/views/admin/EditarPropiedadView.vue
+++ src/views/admin/EditarPropiedadView.vue
@@ -76,6 +76,8 @@ const submit = handleSubmit((values) => {});
 
       <div class="my-5">
         <p class="font-weight-bold">Imagen Actual:</p>
+        <img v-if="image" class="w-50" :src="image" />
+        <img v-else class="w-50" :src="propiedad?.imagen" />
       </div>
 
       <v-text-field
