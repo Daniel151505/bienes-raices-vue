134c818b8780d716d662ef1062f8b95e771287e2
Daniel151505 <daniel150520011@gmail.com>
2023-10-23 19:09:14 -0500

    Iterando y mostrando las propiedades

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -8,4 +8,25 @@ const { propiedadesCollection } = usePropiedades();
   <v-btn color="blue" variant="flat" :to="{ name: 'nueva-propiedad' }">
     Nueva Propiedad
   </v-btn>
+  <v-card class="mx-auto mt-10 flat">
+    <v-list>
+      <v-list-item
+        v-for="propiedad in propiedadesCollection"
+        :key="propiedad.id"
+      >
+        <template v-slot:prepend>
+          <v-list-item-media start="true">
+            <img width="180" :src="propiedad.imagen" />
+          </v-list-item-media>
+        </template>
+        <v-list-item-title>{{ propiedad.titulo }}</v-list-item-title>
+        <v-list-item-subtitle>{{ propiedad.precio }}</v-list-item-subtitle>
+
+        <template>
+          <v-btn color="info" flat clas="mr-2"> Editar </v-btn>
+          <v-btn color="red-darken-3" flat> Eliminar </v-btn>
+        </template>
+      </v-list-item>
+    </v-list>
+  </v-card>
 </template>
