97331d147a5adce4646c2833eb0d57561742edde
Daniel151505 <daniel150520011@gmail.com>
2023-10-23 21:01:56 -0500

    Consultando la Base de datos y mostrando propiedades como Cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+import usePropiedades from "../composables/usePropiedades";
+import PropiedadVue from "../components/Propiedad.vue";
+
+const { propiedadesCollection } = usePropiedades();
+</script>
 
 <template>
-  <main>
-    <h1 class="text-center">Bienes Raices</h1>
-  </main>
+  <h1 class="text-center">Nuestras Propiedades</h1>
+  <v-card flat class="py-10">
+    <v-row>
+      <PropiedadVue
+        v-for="propiedad in propiedadesCollection"
+        :key="propiedad.id"
+        :propiedad="propiedad"
+      />
+    </v-row>
+  </v-card>
 </template>
