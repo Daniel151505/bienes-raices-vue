c82a2dd938a6e7337a6f5a09f3dfdf2391ce8fb8
Daniel151505 <daniel150520011@gmail.com>
2023-10-23 19:01:42 -0500

    Obteniendo las propiedades con VueFire y Firebase

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -1,4 +1,8 @@
-<script setup></script>
+<script setup>
+import usePropiedades from "../../composables/usePropiedades";
+
+const { propiedadesCollection } = usePropiedades();
+</script>
 <template>
   <h2 class="text-center text-h3 my-5 font-weight-bold">Admin Panel</h2>
   <v-btn color="blue" variant="flat" :to="{ name: 'nueva-propiedad' }">
