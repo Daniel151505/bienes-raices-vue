896239c65ea2534496dba52c9280ce9590f86b24
Daniel151505 <daniel150520011@gmail.com>
2023-10-28 22:48:44 -0500

    Filtrando propiedades

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,15 +3,21 @@ import usePropiedades from "../composables/usePropiedades";
 import PropiedadVue from "../components/Propiedad.vue";
 import { propertyPrice } from "../helpers";
 
-const { propiedadesCollection } = usePropiedades();
+const { alberca, propiedadesFiltradas } = usePropiedades();
 </script>
 
 <template>
   <h1 class="text-center">Nuestras Propiedades</h1>
   <v-card flat class="py-10">
+    <v-card-title class="text-h5">
+      Búsqueda
+      <span>
+        <v-checkbox label="Alberca" v-model="alberca" />
+      </span>
+    </v-card-title>
     <v-row>
       <PropiedadVue
-        v-for="propiedad in propiedadesCollection"
+        v-for="propiedad in propiedadesFiltradas"
         :key="propiedad.id"
         :propiedad="propiedad"
         :price="propertyPrice"
